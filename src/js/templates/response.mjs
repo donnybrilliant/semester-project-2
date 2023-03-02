4c251cd6954426368a2913994d8899a8e8a3d100
donnybrilliant <daniel.vier@gmail.com>
2023-03-03 00:22:30 +0100

    Create simple response handler

---
+++ src/js/templates/response.mjs
@@ -0,0 +1,11 @@
+export function responseMessage(data, color) {
+  const container = document.createElement("div");
+  container.classList.add("alert", "alert-" + color);
+  container.innerText = data;
+  return container;
+}
+
+export function renderResponseMessage(data, parent, color) {
+  const response = responseMessage(data, color);
+  parent.append(response);
+}
