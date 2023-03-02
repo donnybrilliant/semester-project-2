65bff4ee5991830d8ff21483352ed3e194abc783
donnybrilliant <daniel.vier@gmail.com>
2023-03-03 00:18:07 +0100

    Create read listings api call

---
+++ src/js/api/listings/read.mjs
@@ -0,0 +1,32 @@
+import { URL } from "../constants.mjs";
+import { authFetch } from "../authFetch.mjs";
+
+export async function readAll(
+  sort = "created",
+  order = "desc",
+  offset = 0,
+  limit = 100,
+  active = "true"
+) {
+  const readListingsURL = `${URL}/listings?_seller=true&_bids=true&sort=${sort}&sortOrder=${order}&offset=${offset}&limit=${limit}&_active=${active}`;
+
+  const response = await authFetch(readListingsURL);
+  if (!response.ok) {
+    console.log(response); // render something here . responseHandler
+  }
+  return response.json();
+}
+
+export async function read(id) {
+  if (!id) {
+    throw new Error("Requires a listing ID"); // render something here?
+  }
+
+  const readListingURL = URL + "/listings/" + id + "?_seller=true&_bids=true";
+
+  const response = await authFetch(readListingURL);
+  if (!response.ok) {
+    console.log(response);
+  }
+  return response.json();
+}
