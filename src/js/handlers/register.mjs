21234f51b4ce75ade7598b08af8a628570e16439
donnybrilliant <daniel.vier@gmail.com>
2023-03-03 00:19:48 +0100

    Create register handler

---
+++ src/js/handlers/register.mjs
@@ -0,0 +1,15 @@
+import { register } from "../api/auth/register.mjs";
+
+export function setRegisterFormListener() {
+  const form = document.querySelector("#registerForm");
+
+  if (form) {
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const form = event.target;
+      const formData = new FormData(form);
+      const user = Object.fromEntries(formData.entries());
+      register(user);
+    });
+  }
+}
